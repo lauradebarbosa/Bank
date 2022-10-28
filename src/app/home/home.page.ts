ee93d0550503732d3552e77c2c96688365fe7acb
lauradebarbosa <laurabarbosarp@gmail.com>
2022-10-28 09:04:13 -0300

    add app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Renderer2 } from '@angular/core';
+import { AnimationController, Animation, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,88 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('blocks') blocks: any
+  @ViewChild('background') background: any
 
-  constructor() {}
+  public options: Array<any> = [
+    {
+      icon: 'person-add-outline', text: 'Indicar amigos'
+    },
+    {
+      icon: 'phone-portrait-outline', text: 'Recarga de Celular'
+    },
+    {
+      icon: 'wallet-outline', text: 'Depositar'
+    },
+    {
+      icon: 'options-outline', text: 'Ajustar limite'
+    },
+    {
+      icon: 'help-circle-outline', text: 'Me ajuda'
+    },
+    {
+      icon: 'barcode-outline', text: 'Pagar'
+    },
+    {
+      icon: 'lock-open-outline', text: 'Bloquear cartão'
+    },
+    {
+      icon: 'card-outline', text: 'Cartão virtual'
+    }
+  ];
 
+  public slidesOptions: any = { slidesPerView: 3.5, freeMode: true };
+
+  public items: Array<any> = [
+    {
+      icon: 'help-circle-outline', text: 'Me Ajuda'
+    },
+    {
+      icon: 'person-outline', text: 'Perfil'
+    },
+    {
+      icon: 'cash-outline', text: 'Configurar conta'
+    },
+    {
+      icon: 'card-outline', text: 'Configurar cartão'
+    },
+    {
+      icon: 'phone-portrait-outline', text: 'Configurações'
+    }
+  ];
+
+  public initialStep: number = 0;
+  private maxTranslate: number;
+  private animation: Animation;
+
+  constructor(
+    private animationCtrl: AnimationController,
+    private platform: Platform,
+    private renderer: Renderer2
+  ) {
+    this.maxTranslate = this.platform.height() - 200;
+  }
+
+  ngAfterViewInit() {
+    this.createAnimation();
+  }
+
+  toggleBlocks() {
+    this.initialStep = this.initialStep === 0 ? this.maxTranslate : 0;
+
+    this.animation.direction(this.initialStep === 0 ? 'reverse' : 'normal').play();
+
+    this.setBackgroundOpacity()
+  }
+
+  createAnimation() {
+    this.animation = this.animationCtrl.create()
+      .addElement(this.blocks.nativeElement)
+      .duration(300)
+      .fromTo('transform', 'translateY(0)', `translateY(${this.maxTranslate}px)`);
+  }
+
+  setBackgroundOpacity() {
+    this.renderer.setStyle(this.background.nativeElement, 'opacity', this.initialStep === 0 ? '0' : '1');
+  }
 }
